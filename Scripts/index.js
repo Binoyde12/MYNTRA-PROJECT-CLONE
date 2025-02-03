6d97d0b37f06ee5888f0c138cce2c810696a8abf
Binoyde12 <babuldebnath0109@gmail.com>
2025-02-03 16:12:22 +0530

    first commit

---
+++ Scripts/index.js
@@ -0,0 +1,72 @@
+let bagItems;
+onLoad();
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem('bagItems');
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+  displayBanner()
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    console.log('I am here');
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+}
+function displayBanner(){
+document.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
+  const images = document.querySelectorAll(".banner_image");
+  const totalImages = images.length;
+
+  function showNextImage() {
+      images[currentIndex].classList.remove("active");
+      currentIndex = (currentIndex + 1) % totalImages;
+      images[currentIndex].classList.add("active");
+  }
+
+  // Show the first image initially
+  images[currentIndex].classList.add("active");
+
+  // Auto-slide every 3 seconds
+  setInterval(showNextImage, 3000);
+});
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
